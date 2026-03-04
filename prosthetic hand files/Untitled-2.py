99aa61ff0f20fd86fff1d40e5661eda0e4f61f21
Saim Sajid <saimsajid0341@gmail.com>
2026-03-04 06:43:37 +0500

    Add hand detection and gesture control scripts
    - Created Untitled-2.py for basic hand detection using cvzone and sending finger data via serial.
    - Developed gesturecontrol.py to enhance hand tracking with bounding box and landmark extraction.
    - Introduced tempCodeRunnerFile.py as a duplicate of Untitled-2.py for testing purposes.

---
+++ prosthetic hand files/Untitled-2.py
@@ -0,0 +1,20 @@
+import cv2
+import cvzone
+
+
+cap = cv2.VideoCapture(1)
+detector = cvzone.HandDetector(maxHands=1, detectionCon=0.7)
+mySerial = cvzone.SerialObject("COM3", 9600, 1)
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+    if lmList:
+        fingers = detector.fingersUp()
+        #print(fingers)
+        mySerial.sendData(fingers)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
